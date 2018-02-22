a3639511041fefeea9ede3837ffc9bd849973700
Efrain Perez Jr <efrainperezjr@live.com>
2018-02-22 17:20:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
+}
